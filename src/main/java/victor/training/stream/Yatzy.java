0d603d389c13bbb560236fd946f3b07a61e1074a
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-28 07:38:12 +0530

    java-streams kata

--- src/main/java/victor/training/stream/Yatzy.java
+++ src/main/java/victor/training/stream/Yatzy.java
@@ -152,16 +152,25 @@ public class Yatzy {
   }
 
   public int four_of_a_kind() {
-    int[] counts = new int[6];
-    counts[dice[0] - 1]++;
-    counts[dice[1] - 1]++;
-    counts[dice[2] - 1]++;
-    counts[dice[3] - 1]++;
-    counts[dice[4] - 1]++;
-    for (int i = 0; i < 6; i++)
-      if (counts[i] >= 4)
-        return (i + 1) * 4;
-    return 0;
+//    int[] counts = new int[6];
+//    counts[dice[0] - 1]++;
+//    counts[dice[1] - 1]++;
+//    counts[dice[2] - 1]++;
+//    counts[dice[3] - 1]++;
+//    counts[dice[4] - 1]++;
+//    for (int i = 0; i < 6; i++)
+//      if (counts[i] >= 4)
+//        return (i + 1) * 4;
+//    return 0;
+
+    return Arrays.stream(dice)
+            .boxed()
+            .collect(groupingBy(Function.identity(), reducing(0, e -> 1, Integer::sum)))
+            .entrySet()
+            .stream().filter(e -> e.getValue() >= 4)
+            .findAny()
+            .map(entry -> entry.getKey() * 4)
+            .orElse(0);
   }
 
   public int three_of_a_kind() {
