290f5f98a9110ca2ca421a1dd956b2021d729bdf
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-28 07:35:16 +0530

    java-streams kata

--- src/main/java/victor/training/stream/Yatzy.java
+++ src/main/java/victor/training/stream/Yatzy.java
@@ -124,23 +124,6 @@ public class Yatzy {
   }
 
   public int two_pair() {
-    int[] counts = new int[6];
-    counts[dice[0] - 1]++;
-    counts[dice[1] - 1]++;
-    counts[dice[2] - 1]++;
-    counts[dice[3] - 1]++;
-    counts[dice[4] - 1]++;
-    int n = 0;
-    int score = 0;
-    for (int i = 0; i < 6; i += 1)
-      if (counts[6 - i - 1] >= 2) {
-        n++;
-        score += (6 - i);
-      }
-    if (n == 2)
-      return score * 2;
-    else
-      return 0;
 //    int[] counts = new int[6];
 //    counts[dice[0] - 1]++;
 //    counts[dice[1] - 1]++;
