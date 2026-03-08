186be229c16c41ecd50dd2fb5ab5416d0af8ea4f
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-08 08:48:43 +0530

    java-streams kata

--- src/main/java/com/learning/stream/Main.java
+++ src/main/java/com/learning/stream/Main.java
@@ -1,16 +1,27 @@
 package com.learning.stream;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1,2,3,4,5);
+        List<Integer> numbersList1 = List.of(1,2,3,4,5);
+        List<Integer> numbersList2 = List.of(3,11,20,5,7,30);
         List<String> words = List.of("java","spring","angular");
 
-        var evenNumbers = numbers.stream().filter(num -> num % 2 == 0).toList();
+        var evenNumbers = numbersList1.stream()
+                .filter(num -> num % 2 == 0).toList();
         System.out.println(evenNumbers);
 
-        var upperCaseWords = words.stream().map(String::toUpperCase).toList();
+        var upperCaseWords = words.stream()
+                .map(String::toUpperCase).toList();
         System.out.println(upperCaseWords);
+
+        var numbersCountGreaterThan10 = numbersList2.stream()
+                .filter(num -> num > 10)
+                .count();
+        System.out.println(numbersCountGreaterThan10);
+
+
     }
 }
